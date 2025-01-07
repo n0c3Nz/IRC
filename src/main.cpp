6e1ea74ed53313f20a209b4883658269603b57f0
c3nz <agapitobusiness@gmail.com>
2025-01-07 13:15:58 +0100

    Fix: Argv pwd

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,28 @@ void checkPort(std::string port)
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
 		std::cerr << "Usage: ./server <port>" << std::endl;
 		return 1;
 	}
+	std::string pwd = argv[2];
+	if (pwd.empty())
+	{
+		std::cerr << "Password cannot be empty" << std::endl;
+		return 1;
+	}
+
+	for (size_t i = 0; i < pwd.length(); i++)
+	{
+		if (!isdigit(pwd[i]))
+		{
+			std::cerr << "Invalid password" << std::endl;
+			exit(1);
+		}
+	}
 	checkPort(argv[1]);
-	Server server(std::stoi(argv[1]), "1234", ":Welcome to the Anon Chat Server");
+	Server server(std::stoi(argv[1]), pwd, ":Welcome to the Anon Chat Server");
 	server.start();
 	server.run();
 	return 0;
